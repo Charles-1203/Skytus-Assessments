e9492925d7a6a4edf61767dcdb06fe55ae9d851d
Charles88 <charlespatel710@gmail.com>
2026-05-15 16:23:30 +0530

    Add files via upload

---
+++ 3. Compare two numbers and print the larger one.py
@@ -0,0 +1,31 @@
+
+# 3. Compare two numbers and print the larger one.
+
+a = 17
+b = 20
+if(a>b):
+    print(a, "is larger than", b)
+else:
+    print(b, "is larger than", a);
+
+
+
+
+
+
+
+
+
+# 4. Write a program to calculate the square and cube of a number.
+
+# 5. Check if two entered numbers are equal.
+
+# 6. Take two numbers and print `True` if both are positive, else `False`.
+
+# 7. Write a program to convert float to integer.
+
+# 8. Take a number as string, convert to int, and multiply by 10.
+
+# 9. Write a program that uses `and` & `or` operators to check multiple conditions.
+
+# 10. Divide two numbers and print the quotient and remainder separately.
